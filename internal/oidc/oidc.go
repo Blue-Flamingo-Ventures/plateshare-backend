3d6d5ec22fd8e6c453f5f72ba65213e8c0c50f9d
Julian Januszka <jjanuszka@ext.believer.gg>
2024-12-30 13:33:55 -0500

    feat: setup endpoint, oidc client for credential swap

---
+++ internal/oidc/oidc.go
@@ -0,0 +1,71 @@
+package oidc
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type TokenResponse struct {
+	IDToken string `json:"id_token"`
+}
+
+type Claims struct {
+	Nickname     string `json:"nickname"`
+	CausalityKey string `json:"causality_key"`
+	jwt.RegisteredClaims
+}
+
+// Makes call to Auth0 to swap user/pass for token
+func FetchIDToken(url string, payload map[string]string) (*TokenResponse, error) {
+	requestBody, err := json.Marshal(payload)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal payload: %v", err)
+	}
+
+	client := &http.Client{Timeout: 10 * time.Second}
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(requestBody))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create request: %v", err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("request failed: %v", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read response body: %v", err)
+	}
+
+	var tokenResponse TokenResponse
+	if err := json.Unmarshal(body, &tokenResponse); err != nil {
+		return nil, fmt.Errorf("failed to parse JSON response: %v", err)
+	}
+
+	return &tokenResponse, nil
+}
+
+// Extracts the causality key and token we need from claims
+func ParseJWT(tokenString string) (*Claims, error) {
+	token, _, err := jwt.NewParser().ParseUnverified(tokenString, &Claims{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse token: %v", err)
+	}
+
+	claims, ok := token.Claims.(*Claims)
+	if !ok {
+		return nil, fmt.Errorf("failed to extract claims from token")
+	}
+
+	return claims, nil
+}
